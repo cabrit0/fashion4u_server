902ca81e16316d3fdeeb7e98fa7de483843eb6c6
cabrit0 <cabrit0o.dev@gmail.com>
2023-01-29 11:24:47 +0000

    posts.populate(name, avatar)

--- controllers/globalPostsController.js
+++ controllers/globalPostsController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 const Post = require("../models/Post");
 
 const globalAllPosts = asyncHandler(async (req, res) => {
-  const posts = await Post.find();
+  const posts = await Post.find().populate("user", ["name", "avatar"]);
   console.log(posts);
   res.status(200).json({ success: true, data: posts });
 });
